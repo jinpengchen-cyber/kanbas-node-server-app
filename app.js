d54e16e164d27192726c8a584ae41a494f976364
jinpengchen-cyber <chen.jinp@northeastern.edu>
2023-12-01 18:46:47 -0500

    connect

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import session from "express-session";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING).catch((error) => console.log(error));
 import UserRoutes from './users/routes.js';
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
